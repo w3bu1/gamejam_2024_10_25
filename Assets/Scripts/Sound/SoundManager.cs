75d89cdb9ee6fa26be918aee81fa67808729a291
Anjaratiana Faneva Rakotonoela <anjrakot@c1r11s5.42antananarivo.mg>
2024-10-27 01:57:51 +0300

    add background sound in game scene

---
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+	public static SoundManager instance;
+
+	[SerializeField] private AudioSource soundFXObject;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+	}
+
+	public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+	{
+		AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+		audioSource.clip = audioClip;
+		audioSource.volume = volume;
+		audioSource.Play();
+		float clipLength = audioSource.clip.length;
+
+		Destroy(audioSource.gameObject, clipLength);
+	}
+}
