34fe55e8c66e75c1a724cc6c809d8e3147c654dc
Anjaratiana Faneva Rakotonoela <anjrakot@c1r11s5.42antananarivo.mg>
2024-10-27 08:11:10 +0300

    add may be Credit scene if we need it

---
+++ Assets/Scripts/CreditControle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditControle : MonoBehaviour
+{
+	public float scrollSpeed = 40f;
+
+	private RecTransform rectTransform;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rectTransform = GetComponent<RectTransform>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+	}
+}
