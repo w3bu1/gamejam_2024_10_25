f85383bf8a9f34b073666db6f7066c1d32fca6ef
Anjaratiana Faneva Rakotonoela <anjrakot@c1r11s5.42antananarivo.mg>
2024-10-27 12:50:55 +0300

    tuto

--- Assets/TutoScript.cs
+++ Assets/TutoScript.cs
@@ -9,10 +9,6 @@ public class TutoScript : MonoBehaviour
 	// get the text title in ui canva
 	[SerializeField] private GameObject _tutoPanel;
 	[SerializeField] private Image[] _tutoImage;
-	[SerializeField] private Image _prev;
-	[SerializeField] private Image _next;
-	[SerializeField] private Image _done;
-	[SerializeField] private Image _skip;
 	private int _tutoIndex = 0;
 
 	// Start is called before the first frame update
@@ -26,20 +22,6 @@ public class TutoScript : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (_tutoIndex == 0)
-			_prev.gameObject.SetActive(false);
-		else
-			_prev.gameObject.SetActive(true);
-		if (_tutoIndex == _tutoImage.Length - 1)
-		{
-			_next.gameObject.SetActive(false);
-			_done.gameObject.SetActive(true);
-		}
-		else
-		{
-			_next.gameObject.SetActive(true);
-			_done.gameObject.SetActive(false);
-		}
 		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.RightArrow))
 		{
 			Next();
@@ -48,7 +30,7 @@ public class TutoScript : MonoBehaviour
 		{
 			Previous();
 		}
-		else if (Input.GetKeyDown(KeyCode.Escape))
+		else if (Input.GetKeyDown(KeyCode.Backspace))
 		{
 			quitTuto();
 		}
@@ -78,7 +60,7 @@ public class TutoScript : MonoBehaviour
 		}
 	}
 
-	private void quitTuto()
+	public void quitTuto()
 	{
 		_tutoPanel.SetActive(false);
 		Time.timeScale = 1;
